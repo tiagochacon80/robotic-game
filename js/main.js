0ea0ceff812a5d837702a7fa817b807cf3a175df
Tiago Chacon <tiagochacon80@gmail.com>
2022-11-07 14:05:58 -0500

    Update statistics

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 const control = document.querySelectorAll("[data-control]")
+const statistics = document.querySelectorAll("[data-statistic]")
+const pieces = { 
+    "arms": { 
+        "strength": 29, 
+        "power": 35, 
+        "stamina": -21, 
+        "speed": -5 
+    },
+    "shield": { 
+        "strength": 41, 
+        "power": 20, 
+        "stamina": 0, 
+        "speed": -20 
+    }, 
+    "cores":{ 
+        "strength": 0, 
+        "power": 7, 
+        "stamina": 48, 
+        "speed": -24
+     }, 
+    "legs":{ 
+        "strength": 27, 
+        "power": 21, 
+        "stamina": -32, 
+        "speed": 42
+     }, 
+    "rocket":{ 
+        "strength": 0, 
+        "power": 28, 
+        "stamina": 0, 
+        "speed": -2 
+    }
+ }
 
 control.forEach( (element) => {
     element.addEventListener("click", (event) => {
-        manipulatesData(event.target.dataset.control, event.target.parentNode)        
+        manipulatesData(event.target.dataset.control, event.target.parentNode)  
+        updateStatistic(event.target.dataset.piece)      
     })
 })
 
@@ -14,5 +48,12 @@ function manipulatesData(operation, control) {
     } else {
         piece.value = parseInt(piece.value) + 1
     }
+}
+
+function updateStatistic(piece) {   
+
+    statistics.forEach( (element) => {
+        element.textContent = parseInt(element.textContent) + pieces[piece][element.dataset.statistic]        
+    })
 }
 
